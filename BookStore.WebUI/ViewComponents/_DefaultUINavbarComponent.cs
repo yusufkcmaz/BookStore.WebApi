ea54f6f0a5b4ad5b776ff09785967e5b31882a0f
YUSUF KAÇMAZ <yusufkacmzz@gmail.com>
2025-06-06 23:43:57 +0300

    AddComponent

---
+++ BookStore.WebUI/ViewComponents/_DefaultUINavbarComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BookStore.WebUI.ViewComponents
+{
+    public class _DefaultUINavbarComponent :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
