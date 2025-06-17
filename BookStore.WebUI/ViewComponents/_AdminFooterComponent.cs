86c4f068dcbc96eb73f088994ca23f452bfc9a78
YUSUF KAÇMAZ <yusufkacmzz@gmail.com>
2025-06-18 02:43:47 +0300

    admin layout viewlere bağlanması

---
+++ BookStore.WebUI/ViewComponents/_AdminFooterComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebUI.ViewComponents
+{
+    public class _AdminFooterComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
