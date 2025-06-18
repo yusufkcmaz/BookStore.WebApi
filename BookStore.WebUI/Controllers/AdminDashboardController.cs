9c2f5ead52314724591c5daaeb7cab0d45ea1dfa
YUSUF KAÇMAZ <yusufkacmzz@gmail.com>
2025-06-18 03:22:40 +0300

    Admin dashboard dahil

---
+++ BookStore.WebUI/Controllers/AdminDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
